1cb77e04c0dc362e349eff1d97e207b36de64055
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-05-20 13:07:18 +0200

    PTE-19: add background image, change button background image

--- menu/Menu.py
+++ menu/Menu.py
@@ -1,29 +1,30 @@
 import pygame
 from pygame.locals import *
-
 from menu.button.Button import Button
 
 pygame.font.init()
 
 
 class Menu:
+
     screen = {}
     PLAY_BUTTON = {}
     NAVIGATION_BUTTON = {}
-    QUIT_BUTTON = {}
-    BACKGROUND_IMAGE = pygame.image.load("menu/menu_background.svg")
-    TEXT_COLOR = (255, 255, 255)
+    EXIT_BUTTON = {}
+    BACKGROUND_IMAGE = pygame.image.load("menu/menu_background.png")
+    MENU_BUTTON = pygame.image.load("menu/button_menu.png")
+    TEXT_COLOR = (228, 36, 48)
     BASE_COLOR = (249, 154, 40)
     HOVERING_COLOR = (230, 205, 87)
-    FONT_HEADER = pygame.font.Font("menu/PressStart2P-Regular.ttf", 100)
-    FONT_OPTION = pygame.font.Font("menu/PressStart2P-Regular.ttf", 60)
+    FONT_HEADER = pygame.font.Font("menu/PressStart2P-Regular.ttf", 80)
+    FONT_OPTION = pygame.font.Font("menu/PressStart2P-Regular.ttf", 40)
 
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
-    def draw_text(self, text, x, y, font):
-        img = font.render(text, True, self.TEXT_COLOR)
+    def draw_text(self, text, x, y, font, text_color):
+        img = font.render(text, True, text_color)
         img_rect = img.get_rect(center=(x, y))
         self.screen.blit(img, img_rect)
 
@@ -34,15 +35,16 @@ class Menu:
 
     def print_window_menu(self):
         self.screen.blit(self.BACKGROUND_IMAGE, (0, 0))
-        self.draw_text("MAIN MENU", 640, 100, self.FONT_HEADER)
-        self.PLAY_BUTTON = Button(None, pos=(640, 250), text_input="PLAY", font=self.FONT_OPTION,
+        self.draw_text("PASS THE EXAM", 640, 100, self.FONT_HEADER, self.TEXT_COLOR)
+        self.PLAY_BUTTON = Button(image=self.MENU_BUTTON, pos=(640, 250), text_input="PLAY", font=self.FONT_OPTION,
                                   base_color=self.BASE_COLOR, hovering_color=self.HOVERING_COLOR)
-        self.NAVIGATION_BUTTON = Button(None, pos=(640, 350), text_input="NAVIGATING", font=self.FONT_OPTION,
+        self.NAVIGATION_BUTTON = Button(image=self.MENU_BUTTON, pos=(640, 400), text_input="CONTROLS",
+                                        font=self.FONT_OPTION,
                                         base_color=self.BASE_COLOR, hovering_color=self.HOVERING_COLOR)
-        self.QUIT_BUTTON = Button(None, pos=(640, 450), text_input="QUIT", font=self.FONT_OPTION,
+        self.EXIT_BUTTON = Button(image=self.MENU_BUTTON, pos=(640, 550), text_input="EXIT", font=self.FONT_OPTION,
                                   base_color=self.BASE_COLOR, hovering_color=self.HOVERING_COLOR)
 
     def handle_menu_loop(self):
-        for button in [self.PLAY_BUTTON, self.NAVIGATION_BUTTON, self.QUIT_BUTTON]:
+        for button in [self.PLAY_BUTTON, self.NAVIGATION_BUTTON, self.EXIT_BUTTON]:
             button.change_color(pygame.mouse.get_pos())
             button.update(self.screen)
--- menu/button/Button.py
+++ menu/button/Button.py
@@ -1,4 +1,5 @@
-class Button():
+class Button:
+
     def __init__(self, image, pos, text_input, font, base_color, hovering_color):
         self.image = image
         self.x_pos = pos[0]
@@ -11,7 +12,7 @@ class Button():
         if self.image is None:
             self.image = self.text
         self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center=(self.x_pos+4, self.y_pos+4))
 
     def update(self, screen):
         if self.image is not None:
