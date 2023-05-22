8b6c73b3d6a894f813cc089bd20dc7a0d51ca396
Iggos <jonskiigor@gmail.com>
2023-05-22 17:23:34 +0200

    PTE-25: Remove unneccessary method and minor code changes

--- player/local_window_player_movement.py
+++ player/local_window_player_movement.py
@@ -7,20 +7,17 @@ SCREEN_HEIGHT = 720
 
 FPS = 60
 
+pygame.init()
+
 
 class LocalWindowPlayerMovement:
     def __init__(self, width, height):
-        pygame.init()
-
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
         self.background_color = (144, 201, 120)
         self.is_running = True
-
-    def tick(self):
-        self.clock.tick(FPS)
 
     def draw_background(self):
         self.screen.fill(self.background_color)
@@ -34,7 +31,7 @@ class LocalWindowPlayerMovement:
 local_window = LocalWindowPlayerMovement(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 while local_window.is_running:
-    local_window.tick()
+    local_window.clock.tick(FPS)
     local_window.draw_background()
     local_window.handle_events()
     pygame.display.update()
