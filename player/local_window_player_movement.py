1e9521db8778cd487fea0f3eda002c07102568b8
Iggos <jonskiigor@gmail.com>
2023-05-22 13:01:21 +0200

    PTE-25: Add LocalWindowPlayerMovement class

---
+++ player/local_window_player_movement.py
@@ -0,0 +1,43 @@
+import pygame
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+FPS = 60
+
+
+class LocalWindowPlayerMovement:
+    def __init__(self, width, height):
+        pygame.init()
+
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.clock = pygame.time.Clock()
+        self.background_color = (144, 201, 120)
+        self.is_running = True
+
+    def tick(self):
+        self.clock.tick(FPS)
+
+    def draw_background(self):
+        self.screen.fill(self.background_color)
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.is_running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.is_running = False
+
+
+local_window = LocalWindowPlayerMovement(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+while local_window.is_running:
+
+    local_window.tick()
+    local_window.draw_background()
+    local_window.handle_events()
+    pygame.display.update()
+
+pygame.quit()
