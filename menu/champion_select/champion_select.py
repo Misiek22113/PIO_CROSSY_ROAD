305fb860c3e3fa7504059ea091656b05727513e6
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-05-22 22:02:37 +0200

    PTE-19: add champion_select UI and keys assets

--- menu/champion_select/champion_select.py
+++ menu/champion_select/champion_select.py
@@ -7,22 +7,44 @@ from menu.window.window import Window
 class ChampionSelect(Window):
     def __init__(self, width, height):
         super().__init__("Champion Select", width, height)
+        self.champion_index = 0
         self.BUTTON_ARROW_RIGHT = None
         self.BUTTON_ARROW_LEFT = None
         self.width = width
         self.height = height
         self.BACK_BUTTON = None
         self.CHAMPION_SELECT_MOUSE_POS = None
+        self.CHAMPIONS = [[pygame.image.load("assets/characters/cute_boy/idle/0.png"), "cute boy"],
+                          [pygame.image.load("assets/characters/engineer/idle/1.png"), "engineer"],
+                          [pygame.image.load("assets/characters/frog/idle/0.png"), "frog"],
+                          [pygame.image.load("assets/characters/girl/idle/0.png"), "girl"],
+                          [pygame.image.load("assets/characters/spiderman/idle/0.png"), "spiderman"],
+                          [pygame.image.load("assets/characters/student/idle/0.png"), "student"]]
 
     def print_champion_select_menu(self):
+        scale = 10
         self.screen.blit(self.BACKGROUND_IMAGE, (0, 0))
-        self.draw_text("Choose your Champion", 640, 100, self.FONT_CHAMPION_SELECT, self.TEXT_COLOR)
+        self.draw_text("Choose your Champion w", 640, 100, self.FONT_CHAMPION_SELECT, self.TEXT_COLOR)
+        self.draw_text(self.CHAMPIONS[self.champion_index][1], 640, 500, self.FONT_CHAMPION_SELECT, self.TEXT_COLOR)
         self.BACK_BUTTON = Button(image=self.MENU_BUTTON, pos=(640, 650), text_input="BACK", font=self.FONT_OPTION,
                                   base_color=self.BASE_COLOR, hovering_color=self.HOVERING_COLOR)
-        self.BUTTON_ARROW_LEFT = Button(image=None, pos=(400, 400), text_input="<", font=self.FONT_OPTION,
+        self.BUTTON_ARROW_LEFT = Button(image=None, pos=(400, 350), text_input="<", font=self.FONT_OPTION,
                                         base_color=self.BASE_COLOR, hovering_color=self.HOVERING_COLOR)
-        self.BUTTON_ARROW_RIGHT = Button(image=None, pos=(880, 400), text_input=">", font=self.FONT_OPTION,
+        self.BUTTON_ARROW_RIGHT = Button(image=None, pos=(880, 350), text_input=">", font=self.FONT_OPTION,
                                          base_color=self.BASE_COLOR, hovering_color=self.HOVERING_COLOR)
+        img = self.CHAMPIONS[self.champion_index][0]
+        img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        img_rect = img.get_rect(center=(640, 350))
+        self.screen.blit(img, img_rect)
+
+    def change_champion_index(self, to_change):
+        max_index = len(self.CHAMPIONS) - 1
+        if max_index >= to_change >= 0:
+            self.champion_index = to_change
+        elif to_change > max_index:
+            self.champion_index = 0
+        elif to_change < 0:
+            self.champion_index = max_index
 
     def handle_champion_select_loop(self):
         while True:
@@ -38,6 +60,10 @@ class ChampionSelect(Window):
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.BACK_BUTTON.check_for_input(self.CHAMPION_SELECT_MOUSE_POS):
                         return "back"
+                    if self.BUTTON_ARROW_RIGHT.check_for_input(self.CHAMPION_SELECT_MOUSE_POS):
+                        self.change_champion_index(self.champion_index + 1)
+                    if self.BUTTON_ARROW_LEFT.check_for_input(self.CHAMPION_SELECT_MOUSE_POS):
+                        self.change_champion_index(self.champion_index - 1)
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
