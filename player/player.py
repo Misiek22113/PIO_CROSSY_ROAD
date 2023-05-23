3b44019006c8f7633bab413fad39f4988f4ed77c
Iggos <jonskiigor@gmail.com>
2023-05-23 21:16:06 +0200

    PTE-21: Add move method

--- player/player.py
+++ player/player.py
@@ -14,3 +14,15 @@ class Player:
         self.moving_right = False
         self.moving_up = False
         self.moving_down = False
+
+    def move(self):
+        if self.moving_left:
+            self.x -= PLAYER_SPEED
+        if self.moving_right:
+            self.x += PLAYER_SPEED
+        if self.moving_down:
+            self.y += PLAYER_SPEED
+        if self.moving_up:
+            self.y -= PLAYER_SPEED
+        self.rect.x = self.x
+        self.rect.y = self.y
