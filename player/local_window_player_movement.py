599b6ca39cadbe471a1630d57e16cf5342fac3d6
Iggos <jonskiigor@gmail.com>
2023-05-23 21:44:24 +0200

    PTE-21: Add player printing and creation methods

--- player/local_window_player_movement.py
+++ player/local_window_player_movement.py
@@ -1,4 +1,5 @@
 import pygame
+from player import create_player
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
@@ -29,10 +30,16 @@ class LocalWindowPlayerMovement:
 
 local_window = LocalWindowPlayerMovement(SCREEN_WIDTH, SCREEN_HEIGHT)
 
+picked_character = "spiderman"
+player = create_player(100, 100, picked_character)
+
 while local_window.is_running:
     local_window.clock.tick(FPS)
     local_window.draw_background()
     local_window.handle_events()
+
+    player.print_player(local_window)
+
     pygame.display.update()
 
     # TODO remember to delete this when real main window will be implemented
--- player/player.py
+++ player/player.py
@@ -1,6 +1,15 @@
 import pygame
 
 PLAYER_SPEED = 4
+PLAYER_SCALE = 5
+
+
+def create_player(x, y, picked_character):
+    player_img = pygame.image.load(f'../assets/characters/{picked_character}/idle/0.png')
+    player_scaled_img = pygame.transform.scale(player_img,
+                                               (int(player_img.get_width() * PLAYER_SCALE),
+                                                int(player_img.get_height() * PLAYER_SCALE)))
+    return Player(x, y, player_scaled_img)
 
 
 class Player:
@@ -26,3 +35,6 @@ class Player:
             self.y -= PLAYER_SPEED
         self.rect.x = self.x
         self.rect.y = self.y
+
+    def print_player(self, local_window):
+        local_window.screen.blit(self.skin, self.rect)
