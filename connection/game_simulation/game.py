3f61c241b89665a2cc2f77dec75bc5661710df43
Hidennnn <240629@edu.p.lodz.pl>
2023-05-29 11:05:57 +0200

    PTE-4: Player moves without FPS drop

---
+++ connection/game_simulation/game.py
@@ -0,0 +1,119 @@
+import random
+import numpy as np
+
+# TODO that is only game simulation - delete later, so it is not refactored. Please, don't read
+
+
+class Game:
+
+    def __init__(self):
+        self.map = np.zeros(shape=(5, 5), dtype='int32')
+        self.player_numbers = np.array([])
+        self.player_positions = [[-1, -1], [-1, -1], [-1, -1]]
+
+    def add_player(self, player_number, position=None):
+        if player_number in self.player_numbers:
+            raise ValueError("Player with this number already exists.")
+
+        if not position:
+            self.add_random_position(player_number)
+        else:
+            if self.map[position[0], position[1]] == 0:
+                raise ValueError("The position is not empty.")
+            else:
+                self.map[position[0], position[1]] = player_number + 1
+                self.player_positions[player_number] = position
+
+        self.player_numbers = np.append(self.player_numbers, player_number)
+
+    def add_random_position(self, player_number):
+        while True:
+            x = random.randint(0, 4)
+            y = random.randint(0, 4)
+            if self.map[x][y] == 0:
+                self.map[x][y] = player_number + 1
+                self.player_positions[player_number] = [x, y]
+                return
+
+    def __str__(self) -> str:
+        map_in_string = ''
+        for row in range(5):
+            for column in range(5):
+                map_in_string = ' '.join([map_in_string, f'{self.map[row, column]} '])
+            map_in_string = ''.join([map_in_string, '\n'])
+        return map_in_string
+
+    def make_move(self, player_number, move):
+        if move == 'w':
+            self.go_up(player_number)
+        elif move == 's':
+            self.go_down(player_number)
+        elif move == 'a':
+            self.go_left(player_number)
+        else:
+            self.go_right(player_number)
+
+        return self.player_positions[player_number]
+
+    def go_up(self, player_number):
+        if self.player_positions[player_number][0] - 1 >= 0:
+            if self.map[self.player_positions[player_number][0] - 1, self.player_positions[player_number][1]] != 0:
+                return
+            else:
+                self.map[self.player_positions[player_number][0], self.player_positions[player_number][1]] = 0
+                self.player_positions[player_number][0] -= 1
+                self.map[self.player_positions[player_number][0], self.player_positions[player_number][1]] = player_number + 1
+
+    def go_down(self, player_number):
+        if self.player_positions[player_number][0] + 1 <= 4:
+            if self.map[self.player_positions[player_number][0] + 1, self.player_positions[player_number][1]] != 0:
+                return
+            else:
+                self.map[self.player_positions[player_number][0], self.player_positions[player_number][1]] = 0
+                self.player_positions[player_number][0] += 1
+                self.map[
+                    self.player_positions[player_number][0], self.player_positions[player_number][1]] = player_number + 1
+
+    def go_right(self, player_number):
+        if self.player_positions[player_number][1] + 1 < 5:
+            if self.map[self.player_positions[player_number][0], self.player_positions[player_number][1] + 1] != 0:
+                return
+            else:
+                self.map[self.player_positions[player_number][0], self.player_positions[player_number][1]] = 0
+                self.player_positions[player_number][1] += 1
+                self.map[self.player_positions[player_number][0], self.player_positions[player_number][
+                    1]] = player_number + 1
+
+    def go_left(self, player_number):
+        if self.player_positions[player_number][1] - 1 >= 0:
+            if self.map[self.player_positions[player_number][0], self.player_positions[player_number][1] - 1] != 0:
+                return
+            else:
+                self.map[self.player_positions[player_number][0], self.player_positions[player_number][1]] = 0
+                self.player_positions[player_number][1] -= 1
+                self.map[self.player_positions[player_number][0], self.player_positions[player_number][
+                    1]] = player_number + 1
+
+    def actual_position(self, player, position):
+        if self.player_positions[player][0] == -1:
+            if position[0] == -1:
+                return
+            self.map[position[0], position[1]] = player + 1
+            self.player_positions[player] = position
+        elif position[0] == -1:
+            self.map[self.player_positions[player][0], self.player_positions[player][1]] = 0
+            self.player_positions[player] = position
+        else:
+            self.map[self.player_positions[player][0], self.player_positions[player][1]] = 0
+            self.map[position[0], position[1]] = player + 1
+            self.player_positions[player] = position
+
+    def get_positions(self):
+        return self.player_positions
+
+    def delete_player(self, player):
+        self.map[self.player_positions[player][0], self.player_positions[player][1]] = 0
+        self.player_positions[player] = [-1, -1]
+        self.player_numbers = np.delete(self.player_numbers, player)
+
+game = Game()
--- src/client.py
+++ src/client.py
@@ -11,23 +11,10 @@ from game_simulation.game import Game
 from src.player.local_window_player_movement import FPS, LocalWindowPlayerMovement, SCREEN_WIDTH, SCREEN_HEIGHT
 from src.player.player import create_player
 
-QUIT = "q"
-CLOSE_SIGNAL = None
-CLOSE_SIGNAL_ERROR = False
-
-SERVER_IS_NOT_CONNECT = None
-
-CLIENT_IS_RUNNING = False
-CLIENT_IS_CLOSING = True
-
 HOST_IP = "127.0.0.1"
 HOST_PORT = 6000
 
-SIZE_OF_CONNECT_MESSAGE = 40
 SIZE_OF_RECV_WITH_POSITIONS = 1000
-
-DISCONNECT_MESSAGE = "NO"
-CLOSING_MESSAGE = b"c"
 
 
 class Client:
@@ -35,9 +22,7 @@ class Client:
     def __init__(self):
         self.server_ip = HOST_IP
         self.server_port = HOST_PORT
-        self.server_socket = SERVER_IS_NOT_CONNECT
-        self.client_status = CLIENT_IS_RUNNING
-        self.map = Game()
+        self.server_socket = None
         pygame.init()
         self.local_window = LocalWindowPlayerMovement(SCREEN_WIDTH, SCREEN_HEIGHT)
         self.player = create_player(100, 100, "spiderman")
@@ -47,51 +32,23 @@ class Client:
                     "moving_up": False,
                     "moving_down": False
                      }
-        self.lock = threading.Lock()
 
     def start_client(self):
-        threading.Thread(target=self.start_connection).start()
-
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.connect((self.server_ip, self.server_port))
         while self.local_window.is_running:
             self.local_window.clock.tick(FPS)
             self.local_window.draw_background()
             self.local_window.handle_events(self.move)
 
-            # self.player.move(self.move)
+            positions = pickle.loads(self.server_socket.recv(SIZE_OF_RECV_WITH_POSITIONS))
+            self.server_socket.sendall(pickle.dumps(self.move))
+            self.player.set_xy(positions[0], positions[1])
             self.player.print_player(self.local_window)
 
             pygame.display.update()
 
         pygame.quit()
-
-    def start_connection(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        try:
-            self.server_socket.connect((self.server_ip, self.server_port))
-        except ConnectionRefusedError:
-            print("Server is offline.")
-            sys.exit()
-
-        connection_message = self.server_socket.recv(SIZE_OF_CONNECT_MESSAGE).decode()
-
-        if connection_message == DISCONNECT_MESSAGE:
-            print("Server is full.")
-            self.server_socket.close()
-            sys.exit()
-
-        threading.Thread(target=self.start_game).start()
-
-    def start_game(self):
-        threading.Thread(target=self.receive_move).start()
-
-    def receive_move(self):
-        while True:
-            tak = time.time()
-            positions = pickle.loads(self.server_socket.recv(SIZE_OF_RECV_WITH_POSITIONS))
-            self.server_socket.sendall(pickle.dumps(self.move))
-            self.player.set_xy(positions[0], positions[1])
-            print(tak - time.time())
 
 # TODO delete later
 client = Client()
