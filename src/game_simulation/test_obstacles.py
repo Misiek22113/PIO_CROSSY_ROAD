1594d1f38e63bc099434fab400f2c594bb1617a3
Iggos <jonskiigor@gmail.com>
2023-06-03 20:33:49 +0200

    PTE-9: Add obstacle generation at randomized position

--- src/game_simulation/test_obstacles.py
+++ src/game_simulation/test_obstacles.py
@@ -1,8 +1,12 @@
 # class with methods only to test obstacles, i.e. collisions, printing, moving etc.
 # TODO move logic behind this to server, when it will accept multiple players in game
+import random
 
 from src.obstacles.obstacle import create_obstacle, Obstacle
 from src.map.map import SCROLL_SPEED
+
+OBSTACLE_Y_GENERATION_LOWER_BOUND = 250
+OBSTACLE_Y_GENERATION_UPPER_BOUND = 700
 
 
 class TestObstacles:
@@ -10,7 +14,9 @@ class TestObstacles:
         self.obstacles = []
 
     def add_obstacle(self):
-        obstacle = create_obstacle("desk", 1100, 500)
+        x = 1400
+        y = random.randint(OBSTACLE_Y_GENERATION_LOWER_BOUND, OBSTACLE_Y_GENERATION_UPPER_BOUND)
+        obstacle = create_obstacle("desk", x, y)
         self.obstacles.append(obstacle)
 
     def handle_obstacles(self, screen):
