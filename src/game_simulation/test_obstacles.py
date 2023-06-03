2860792baba028c93525370c6ac561bd3e6d006b
Iggos <jonskiigor@gmail.com>
2023-06-03 20:50:11 +0200

    PTE-9: Add finish line generation after specified time

--- src/game_simulation/test_obstacles.py
+++ src/game_simulation/test_obstacles.py
@@ -8,15 +8,23 @@ from src.map.map import SCROLL_SPEED
 OBSTACLE_Y_GENERATION_LOWER_BOUND = 250
 OBSTACLE_Y_GENERATION_UPPER_BOUND = 700
 
+FINISH_LINE_Y_OFFSET = 532.91
 
 class TestObstacles:
     def __init__(self):
         self.obstacles = []
 
-    def add_obstacle(self):
+    def add_obstacle(self, generate_finish_line=False):
         x = 1400
-        y = random.randint(OBSTACLE_Y_GENERATION_LOWER_BOUND, OBSTACLE_Y_GENERATION_UPPER_BOUND)
-        obstacle = create_obstacle("desk", x, y)
+
+        obstacle = None
+        if not generate_finish_line:
+            y = random.randint(OBSTACLE_Y_GENERATION_LOWER_BOUND, OBSTACLE_Y_GENERATION_UPPER_BOUND)
+            obstacle = create_obstacle("desk", x, y)
+        else:
+            y = FINISH_LINE_Y_OFFSET
+            obstacle = create_obstacle("+finish_line", x, y)
+
         self.obstacles.append(obstacle)
 
     def handle_obstacles(self, screen):
--- src/obstacles/obstacle.py
+++ src/obstacles/obstacle.py
@@ -2,21 +2,30 @@ import pygame
 
 OBSTACLE_SCALE = 5
 
-obstacles_types = {  # if _ is first char in obstacle type name, then it's deadly
+obstacles_types = {  # if _ is first char in obstacle type name, then it's deadly, if it's +, then that's finish line
     "desk": f"obstacles/assets/desk.png",
-    "_integral": f"obstacles/assets/integral.png"
+    "_integral": f"obstacles/assets/integral.png",
+
+    "+finish_line": f"obstacles/assets/finish_line.png"
 }
 
 
 def create_obstacle(obstacle_type, x, y):
     img_src = obstacles_types.get(obstacle_type, obstacles_types["desk"])
     img = pygame.image.load(img_src)
-    scaled_img = pygame.transform.scale(img,
-                                        (int(img.get_width() * OBSTACLE_SCALE),
-                                         int(img.get_height() * OBSTACLE_SCALE)))
+
+    if obstacle_type != "+finish_line":
+        scaled_img = pygame.transform.scale(img,
+                                            (int(img.get_width() * OBSTACLE_SCALE),
+                                             int(img.get_height() * OBSTACLE_SCALE)))
+    else:
+        scaled_img = img
+
     new_obstacle = Obstacle(scaled_img, x, y)
     if obstacle_type[0] == '_':
         new_obstacle.is_deadly = True
+    elif obstacle_type[0] == '+':
+        new_obstacle.is_finish_line = True
 
     return new_obstacle
 
@@ -29,6 +38,7 @@ class Obstacle:
         self.rect = self.img.get_rect()
         self.rect.center = (x, y)
         self.is_deadly = False
+        self.is_finish_line = False
 
     def print_obstacle(self, screen):
         screen.blit(self.img, self.rect)
